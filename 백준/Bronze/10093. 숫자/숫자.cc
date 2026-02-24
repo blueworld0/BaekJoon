2a373bb480ab585050cec1cc8bba454d63a66188
blueworld0 <105661038+blueworld0@users.noreply.github.com>
2026-02-24 19:38:11 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long  a, b;
+    cin >> a >> b;
+
+    if (a < b) {
+        cout << b - a - 1 << "\n";
+
+        for (long long i = a + 1; i < b; i++) {
+            cout << i;
+            if (i != b - 1) cout << " ";
+        }
+    }
+    else if (a > b) {
+        cout << a - b - 1 << "\n";
+        for (long long i = b + 1; i < a; i++) {
+            cout << i;
+            if (i != a - 1) cout << " ";
+        }
+    }
+    else cout << 0;
+
+    return 0;
+}
