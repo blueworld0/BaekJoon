56a472547e9fc76026640cad53268c6f58efbe09
blueworld0 <105661038+blueworld0@users.noreply.github.com>
2026-03-03 13:11:19 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--) {
+        for (int j = n - i; j > 0; j--) {
+            cout << " ";
+        }
+        for (int k = 0; k < 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = n - i; j > 0; j--) {
+            cout << " ";
+        }
+        for (int k = 0; k < 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
