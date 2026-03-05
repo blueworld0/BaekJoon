6160ba10905b1f7ab9f7cf651e60f5d3bb7ff605
blueworld0 <105661038+blueworld0@users.noreply.github.com>
2026-03-05 10:24:18 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        arr[i] = 0;
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+        arr[str[i] - '0']++;
+    }
+
+    int mid = ((arr[6] + arr[9]) / 2) + ((arr[6] + arr[9]) % 2);
+    arr[6] = mid;
+    arr[9] = mid;
+
+    cout <<*max_element(arr, arr + 10);
+    return 0;
+}
